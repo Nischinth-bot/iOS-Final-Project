afc8936602b41741ca49c9466ba4c3860db7096a
Nischinth Murari <murarins@appstate.edu>
2020-11-29 03:19:36 -0500

    Initial Commit

---
+++ GammaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GammaApp
+//
+//  Created by user180610 on 11/29/20.
+//  Copyright © 2020 Nischinth Murari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
